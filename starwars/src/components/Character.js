667edc15b6a39517a86ac309b1ab557ee43bffb5
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-07-17 09:48:02 -0700

    styled components installed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,27 @@
 // Write your Character component here
 import React from 'react'
+import Style from 'styled-components'
+
+const CharacterDiv = Style.div `
+    
+    border: 2px solid grey;
+    margin-left: 500px;
+    margin-right: 500px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+
+    h2{
+        color:white;
+    }
+`
 
 const Character = (props) => {
     console.log(props)
     
     return (
-        <div>
+        <CharacterDiv>
             <h2>{props.name.name}</h2>
-        </div>
+        </CharacterDiv>
     )
 }
 
